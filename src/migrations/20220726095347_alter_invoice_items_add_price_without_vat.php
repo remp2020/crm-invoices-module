a9677132240274f13f21ef5366c545293f12e956
Lubos Michalik <lubos.michalik@projektn.sk>
2022-08-08 14:44:13 +0200

    Fix wrongly committed migrations for `invoice_items` and `payment_items` table.
    remp/crm#1323

--- src/migrations/20220726095347_alter_invoice_items_add_price_without_vat.php
+++ src/migrations/20220726095347_alter_invoice_items_add_price_without_vat.php
@@ -10,21 +10,20 @@ class AlterInvoiceItemsAddPriceWithoutVat extends AbstractMigration
             ->addColumn('price_without_vat', 'decimal', ['after'=> 'price', 'scale' => 2, 'precision' => 10, 'null' => true])
             ->update();
 
-        // formula for amount without vat is incorrect; this was fixed by following migration FixPaymentItemsAmountWithoutVat
         $sql = <<<SQL
 UPDATE `invoice_items` 
 SET `price_without_vat` = ROUND(`price` / (1 + (`vat`/100)), 2); 
 SQL;
         $this->execute($sql);
 
-        $this->table('payment_items')
-            ->changeColumn('amount_without_vat', 'decimal', ['scale' => 2, 'precision' => 10, 'null' => false])
+        $this->table('invoice_items')
+            ->changeColumn('price_without_vat', 'decimal', ['scale' => 2, 'precision' => 10, 'null' => false])
             ->update();
     }
 
     public function down()
     {
-        $this->table('payment_items')
+        $this->table('invoice_items')
             ->removeColumn('price_without_vat')
             ->update();
     }
