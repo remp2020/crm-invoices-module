6d03f186003c229edd9372783b26ea99e4b1688e
Miroslav Svitok <mirosvitok@gmail.com>
2024-03-27 14:56:06 +0000

    Fix type mismatch in Latte variable $user in nette/application 3.2+
    $user variable renamed to $userRow
    
    remp/crm#3135

--- src/Presenters/InvoicesAdminPresenter.php
+++ src/Presenters/InvoicesAdminPresenter.php
@@ -208,7 +208,7 @@ class InvoicesAdminPresenter extends AdminPresenter
 
         $this->template->pdf = $pdf;
         $this->template->paymentId = $payment->id;
-        $this->template->dbUser = $payment->user;
+        $this->template->userRow = $payment->user;
         $this->template->invoice = $invoice;
     }
 
